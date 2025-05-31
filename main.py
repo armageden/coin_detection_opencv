63c7f1445a2ab1bb7d75daf7aafb689294e75953
farhan_laPTOP <farhantanvir277@gamil.com>
2025-06-01 03:02:31 +0600

    Fix typo in comment and ensure proper display of blurred and edge-detected images

--- main.py
+++ main.py
@@ -8,5 +8,18 @@ def coin_cnt(img_path):
     gry_scl=cv2.cvtColor(img ,cv2.COLOR_BGR2GRAY) #gray scaling hope fully works...
     
     blur_img=cv2.GaussianBlur(gry_scl,(11,11),0) #blurring the image
-    # increaased the values for added clarity for the detection as from test runs I saw the coins need more room to work neatly...
+    # increased the values for added clarity for the detection as from test runs I saw the coins need more room to work neatly...
+    
+    # to display the result at this point
+    cv2.imshow("blurred image",blur_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()# press any key to close the display window
+    
+    edges_img=cv2.Canny(blur_img,50,156) # might need to change to detect more edges for a better result
+    # to display the result at this point
+    cv2.imshow("edges image",edges_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()# press any key to close the display window
+    
+    
     
