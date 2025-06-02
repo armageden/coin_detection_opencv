9d638948bc15f17759fa360bc37a1e0bdd03f834
farhan_laPTOP <farhantanvir277@gamil.com>
2025-06-02 23:06:28 +0600

    Add contour detection and filtering for coin detection in images

--- main.py
+++ main.py
@@ -21,5 +21,52 @@ def coin_cnt(img_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()# press any key to close the display window
     
+    contour,f=cv2.findContours(edges_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # finding the contours of the image
+    if contour is None:
+        raise ValueError("No contours found in the image")
+        return
+    else:
+        print(f"Number of contours found: {len(contour)}")
+    # to display the result at this point
+    
+    # to compare contours with the area of the coins in the original image
+    img_copy=img.copy()
+    coin_cnt=0
+    
+    #filtering the contours based on area
+    for i ,c in enumerate(contour):
+        area=cv2.contourArea(c)
+        perm=cv2.arcLength(c,True)
+        #apox for a poligon
+        approx=cv2.approxPolyDP(c,0.02*perm,True)
+        
+        min,max=1000,10000 #might need adjustment based on the image size
+        circule=0 # to check if the contour is a circle but this   is for taking the value... please remember future me...
+        if perm>0:
+            circule=4*3.14*area/(perm*perm)
+        #flag the detected contour as a coin...ig
+        coin=True
+        if not (min<area<max):
+            coin=False
+        if circule<0.66 or circule>1.3:
+            coin=False
+        if len(approx)<6:# this might need to change depending on the accuracy..
+            coin=False
+        if coin:
+            cv2.drawContours(img_copy, [c], -1, (0, 255, 0), 2)  #detection on the image(green :3)
+            coin_cnt+=1
+    #display the result at this point
+    print("\n Starting to display the result...")
+    cv2.imshow("detected coins",img_copy)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()  # press any key to close the display window
+    print(f"Total number of coins detected: {coin_cnt}")
+    
+if __name__ == "__main__":
+    img_path = ""
+    # Provide the path to your image here
+    coin_cnt(img_path)  # Call the function with the image path
+    # Example: coin_cnt("path/to/your/image.jpg")
+        
     
     
